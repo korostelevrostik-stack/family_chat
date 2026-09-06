b00ef765ff9297a04ab8aacf9fb7643d6f40a215
korostelevrostik-stack <korostelevrostik@gmail.com>
2026-09-06 22:17:33 +0500

    Refactor user management and messaging system

--- server.js
+++ server.js
@@ -8,57 +8,115 @@ const io = new Server(server);
 
 app.use(express.static('public'));
 
-// Храним пользователей и их сообщения
-const users = {}; // { socketId: { name, contacts: [names] } }
-const messages = {}; // { "Папа-Мама": [ { from, text, time } ] }
+// База данных в памяти
+const accounts = {}; // { login: { name, password, online } }
+const messages = {}; // { "login1-login2": [ { from, text, time } ] }
 
 io.on('connection', (socket) => {
-  console.log('✅ Кто-то подключился');
+  console.log('✅ Новое подключение');
 
-  // Регистрация пользователя
-  socket.on('register', (name) => {
-    users[socket.id] = { name, contacts: [] };
-    socket.broadcast.emit('userJoined', name);
-    // Отправляем новому пользователю список всех уже зарегистрированных
-    const allUsers = Object.values(users).map(u => u.name);
-    socket.emit('userList', allUsers);
+  // Регистрация
+  socket.on('register', ({ login, password, name }) => {
+    if (accounts[login]) {
+      socket.emit('registerError', 'Логин уже занят!');
+      return;
+    }
+    accounts[login] = { name, password, online: true };
+    socket.login = login;
+    socket.name = name;
+    socket.emit('registerSuccess', { login, name });
+    broadcastUsers();
   });
 
-  // Получить историю диалога с конкретным пользователем
-  socket.on('getHistory', ({ withUser }) => {
-    const myName = users[socket.id]?.name;
-    if (!myName) return;
-    const key = [myName, withUser].sort().join('-');
+  // Вход
+  socket.on('login', ({ login, password }) => {
+    if (!accounts[login]) {
+      socket.emit('loginError', 'Аккаунт не найден!');
+      return;
+    }
+    if (accounts[login].password !== password) {
+      socket.emit('loginError', 'Неверный пароль!');
+      return;
+    }
+    socket.login = login;
+    socket.name = accounts[login].name;
+    accounts[login].online = true;
+    socket.emit('loginSuccess', { login, name: accounts[login].name });
+    broadcastUsers();
+  });
+
+  // Сменить отображаемое имя
+  socket.on('changeName', ({ newName }) => {
+    if (!socket.login) return;
+    accounts[socket.login].name = newName;
+    socket.name = newName;
+    broadcastUsers();
+  });
+
+  // Получить список пользователей
+  socket.on('getUsers', () => {
+    const users = Object.keys(accounts).map(login => ({
+      login,
+      name: accounts[login].name,
+      online: accounts[login].online
+    }));
+    socket.emit('userList', users);
+  });
+
+  // Получить историю диалога
+  socket.on('getHistory', ({ withLogin }) => {
+    if (!socket.login) return;
+    const key = [socket.login, withLogin].sort().join('-');
     const history = messages[key] || [];
-    socket.emit('history', { withUser, messages: history });
+    socket.emit('history', { withLogin, messages: history });
   });
 
   // Отправить личное сообщение
-  socket.on('sendPrivate', ({ to, text }) => {
-    const myName = users[socket.id]?.name;
-    if (!myName || !to) return;
+  socket.on('sendPrivate', ({ toLogin, text }) => {
+    if (!socket.login) return;
     const time = new Date().toLocaleTimeString('ru-RU', { hour: '2-digit', minute: '2-digit' });
-    const key = [myName, to].sort().join('-');
+    const key = [socket.login, toLogin].sort().join('-');
     if (!messages[key]) messages[key] = [];
-    messages[key].push({ from: myName, text, time });
+    messages[key].push({ from: socket.login, text, time });
 
-    // Отправляем получателю
-    const recipientSocket = Object.keys(users).find(id => users[id].name === to);
-    if (recipientSocket) {
-      io.to(recipientSocket).emit('newPrivateMessage', { from: myName, text, time });
+    // Отправляем получателю, если он онлайн
+    const recipient = Object.keys(io.sockets.sockets).find(id => {
+      return io.sockets.sockets[id].login === toLogin;
+    });
+    if (recipient) {
+      io.to(recipient).emit('newPrivateMessage', { 
+        from: socket.login, 
+        fromName: socket.name, 
+        text, 
+        time 
+      });
     }
-    // Отправляем отправителю (для обновления его чата)
-    socket.emit('newPrivateMessage', { from: myName, text, time });
+    // Отправляем отправителю
+    socket.emit('newPrivateMessage', { 
+      from: socket.login, 
+      fromName: socket.name, 
+      text, 
+      time 
+    });
   });
 
   // Отключение
   socket.on('disconnect', () => {
-    const name = users[socket.id]?.name;
-    if (name) {
-      socket.broadcast.emit('userLeft', name);
-      delete users[socket.id];
+    if (socket.login) {
+      accounts[socket.login].online = false;
+      broadcastUsers();
     }
   });
+
+  // Рассылка списка пользователей всем
+  function broadcastUsers() {
+    const users = Object.keys(accounts).map(login => ({
+      login,
+      name: accounts[login].name,
+      online: accounts[login].online
+    }));
+    io.emit('userList', users);
+  }
 });
 
 server.listen(3000, () => {
